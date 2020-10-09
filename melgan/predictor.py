1769e849dcf8653464cff0b601e9861535ec3ad6
Sean Tempesta <sean.tempesta@gmail.com>
2020-10-09 13:59:46 +0800

    moved melgan into a package like format

---
+++ melgan/predictor.py
@@ -0,0 +1,62 @@
+import os
+import glob
+import tqdm
+import torch
+import argparse
+from scipy.io.wavfile import write
+import numpy as np
+from melgan.model.generator import Generator
+from melgan.utils.hparams import HParam, load_hparam_str
+from melgan.utils.pqmf import PQMF
+from melgan.denoiser import Denoiser
+
+MAX_WAV_VALUE = 32768.0
+
+def init(config, checkpoint_path, device="cuda"):
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    if config is not None:
+        hp = HParam(config)
+    else:
+        hp = load_hparam_str(checkpoint['hp_str'])
+
+    model = Generator(hp.audio.n_mel_channels, hp.model.n_residual_layers,
+                        ratios=hp.model.generator_ratio, mult = hp.model.mult,
+                        out_band = hp.model.out_channels).to(device)
+    model.load_state_dict(checkpoint['model_g'])
+    model.eval(inference=True)
+    return hp, model
+
+
+def predict(hp, model, mel, denoise=None, device="cuda"):
+    with torch.no_grad():
+        if len(mel.shape) == 2:
+            mel = mel.unsqueeze(0)
+        mel = mel.to(device)
+        audio = model.inference(mel)
+        # For multi-band inference
+        if hp.model.out_channels > 1:
+            pqmf = PQMF(device=device)
+            audio = pqmf.synthesis(audio).squeeze(0) #.view(-1)
+  #      audio = audio.squeeze(0)  # collapse all dimension except time axis
+        if denoise:
+            denoiser = Denoiser(model, device=device).to(device)
+            audio = denoiser(audio, denoise).mean(0)
+        audio = audio.squeeze()
+        audio = audio[:-(hp.audio.hop_length*10)]
+        audio = MAX_WAV_VALUE * audio
+        audio = audio.clamp(min=-MAX_WAV_VALUE, max=MAX_WAV_VALUE-1)
+        audio = audio.short()
+        audio = audio.cpu().detach().numpy()
+        return audio
+
+
+def repl_test():
+    config = './melgan/config/mb_melgan.yaml'
+    checkpoint_path = './melgan/mb_melgan_901be72_0600.pt'
+    device = "cpu"
+
+    hp, model = init(config, checkpoint_path, device=device)
+
+    mel = torch.randn(1, 80, 106).to(device)
+    denoise = 0.05
+    audio = predict(hp, model, mel, denoise=denoise, device=device)
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import Extension, setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="melgan",  # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=['melgan'],
+    install_requires=[
+        'numpy',
+        'torch',
+        'unidecode',
+        'inflect',
+        'scipy',
+        'librosa',
+        'Cython',
+        'pillow',
+        'pyyaml',
+        'tqdm',
+        'tensorboardX',
+        'matplotlib'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
