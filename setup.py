f2000d120eebd30016c4471a6a6f5a399de7600c
Sean Tempesta <sean.tempesta@gmail.com>
2020-10-09 18:34:16 +0800

    maybe this will work better for package importing?

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=['melgan'],
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'torch',
